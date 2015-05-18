5fddf6d638261b910f417b83e59cf51efef2cb11
varunsudan <varun.sudan@gmail.com>
2015-05-18 13:07:32 -0500

    Update NQuene.java
    Made it work. Need to figure out completely

--- NQuene.java
+++ NQuene.java
@@ -1,37 +1,29 @@
-package com.varun.nquene;
+import java.util.Arrays;
 
-public class NQuene {
+public class NQueens {
 	
 	private static int[] columns = null;
 	
 	public static void main(String [] args) {
-		int n = 4;
+		int n = 2;
 		columns = new int[n];
 		nQueens(0,n);
 	}
 	
 	private static void nQueens(int row, int n) {
 		
-		for(int i = row; i < n; i++) {
+		for(int i = 0; i < n; i++) {
 			if(isPlaced(row, i)) {
-				//System.out.println("(" + row + " , " + i +" )");
 				columns[row] = i;
-				if(row == (n-1)) return;
-				nQueens(row+1, n);
-			} else {
-				if((row-1) >= 0) {
-					nQueens(row-1,n);
+				if(row == (n-1)) {
+					System.out.println("Current state in array = " + Arrays.toString(columns));
+					return ;
+				} else {
+					nQueens(row+1, n);
 				}
 				
-			}
+			} 
 		}
-		System.out.println("result is = " );
-	
-//		int rowNum = 0;
-//		for(int column : columns) {
-//			System.out.println("row = " + rowNum+", column  =" + column);
-//			rowNum++;
-//		}
 	}
 	
 	private static boolean isPlaced(int row, int column) {
