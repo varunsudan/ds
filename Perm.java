ccc5e7f0dbfdd5591b1c5ea9d5bcaf392c118765
varunsudan <Varun Sudan>
2015-05-17 22:15:07 -0500

    Perm java

---
+++ Perm.java
@@ -0,0 +1,32 @@
+package com.varun.perm;
+
+import java.util.Arrays;
+
+public class Perm {
+	
+	public static int counter = 1;
+	
+	public static void main(String [] args) {
+		perm(new int[]{1,2,3,4}, 0, 4);
+	}
+	
+	public static void perm(int [] input, int startIndex, int size) {
+		
+		if(startIndex == (size-1)) {
+			System.out.println(counter+"-- Perm ---->  "+ Arrays.toString(input));
+			counter++;
+		} else {
+			for(int index = startIndex; index < size ;index++) {
+				int temp = input[startIndex];
+				input[startIndex] = input[index];
+				input[index] = temp;
+				perm(input, startIndex+1, size);
+				temp = input[index];
+				input[index] = input[startIndex];
+				input[startIndex] = temp;
+			}
+		}
+		
+	}
+
+}
